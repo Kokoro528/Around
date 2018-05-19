3828a87b3cb38752c5ee4d950f7c8023988ccf02
kokoro528 <eleanorbutemily@gmail.com>
2018-05-19 18:10:19 -0500

    bigtable added

--- service/main.go
+++ service/main.go
@@ -1,23 +1,25 @@
-package main
 
+package main
 import (
 	elastic "gopkg.in/olivere/elastic.v3"
 	"fmt"
+	"reflect"
 	"net/http"
 	"encoding/json"
-	"log"
 	"strconv"
-	"reflect"
+	"log"
+	"cloud.google.com/go/bigtable"
 	"github.com/pborman/uuid"
-	"strings"
+	"context"
 
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
 )
-
 type Location struct {
 	Lat float64 `json:"lat"`
 	Lon float64 `json:"lon"`
 }
-
 type Post struct {
 	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
 	User     string `json:"user"`
@@ -25,17 +27,17 @@ type Post struct {
 	Location Location `json:"location"`
 }
 
+var mySigningKey = []byte("secret")
+
 const (
 	INDEX = "around"
 	TYPE = "post"
 	DISTANCE = "200km"
-	// Needs to update
 	PROJECT_ID = "around-204004"
 	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.231.170.185:9200"
+	ES_URL = "http://35.196.146.91:9200"
 )
-
 func main() {
 	// Create a client
 	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
@@ -43,7 +45,6 @@ func main() {
 		panic(err)
 		return
 	}
-
 	// Use the IndexExists service to check if a specified index exists.
 	exists, err := client.IndexExists(INDEX).Do()
 	if err != nil {
@@ -52,42 +53,51 @@ func main() {
 	if !exists {
 		// Create a new index.
 		mapping := `{
-                    "mappings":{
-                           "post":{
-                                  "properties":{
-                                         "location":{
-                                                "type":"geo_point"
-                                         }
-                                  }
-                           }
-                    }
-             }
-             `
+                       "mappings":{
+                               "post":{
+                                       "properties":{
+                                               "location":{
+                                                       "type":"geo_point"
+                                               }
+                                       }
+                               }
+                       }
+               }
+               `
 		_, err := client.CreateIndex(INDEX).Body(mapping).Do()
 		if err != nil {
 			// Handle error
 			panic(err)
 		}
 	}
+	r := mux.NewRouter()
 
-	fmt.Println("started-service")
-	http.HandleFunc("/post", handlerPost)
-	http.HandleFunc("/search", handlerSearch)
+	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return mySigningKey, nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+	r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
+	r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+
+	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received one request for search")
 	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
 	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
-
 	// range is optional
 	ran := DISTANCE
 	if val := r.URL.Query().Get("range"); val != "" {
 		ran = val + "km"
 	}
-
-	fmt.Printf( "Search received: %f %f %s\n", lat, lon, ran)
 
 	// Create a client
 	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
@@ -123,13 +133,11 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	// However, it ignores errors in serialization.
 	var typ Post
 	var ps []Post
-	for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // instance of
-		p := item.(Post) // p = (Post) item
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) {
+		p := item.(Post)
 		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
-		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
-		if !containsFilteredWords(&p.Message) {
-			ps = append(ps,p)
-		}
+		// TODO(vincent): Perform filtering based on keywords such as web spam etc.
+		ps = append(ps, p)
 
 	}
 	js, err := json.Marshal(ps)
@@ -139,12 +147,15 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
 	// Parse from body of request to get a json object.
+	user := r.Context().Value("user")
+	claims := user.(*jwt.Token).Claims
+	username := claims.(jwt.MapClaims)["username"]
+
 	fmt.Println("Received one post request")
 	decoder := json.NewDecoder(r.Body)
 	var p Post
@@ -152,20 +163,16 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 		return
 	}
-	id := uuid.New()
-	// Save to ES.
-	saveToES(&p, id)
+	p.User = username.(string)
 
-}
-
-// Save a post to ElasticSearch
-func saveToES(p *Post, id string) {
 	// Create a client
 	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
 	if err != nil {
 		panic(err)
 		return
 	}
+
+	id := uuid.New()
 
 	// Save it to index
 	_, err = es_client.Index().
@@ -181,17 +188,121 @@ func saveToES(p *Post, id string) {
 	}
 
 	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+
+	ctx := context.Background()
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+	       panic(err)
+	       return
+	}
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+	       panic(err)
+	       return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+
 }
 
-func containsFilteredWords(s *string) bool {
-	filteredWords := []string{
-		"fuck",
-		"150",
-	}
-	for _, word := range filteredWords {
-		if strings.Contains(*s, word) {
-			return true
-		}
-	}
-	return false
-}
+
+//
+//func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+//	client, err := storage.NewClient(ctx)
+//	if err != nil {
+//		return nil, nil, err
+//	}
+//	defer client.Close()
+//
+//	bucket := client.Bucket(bucketName)
+//	// Next check if the bucket exists
+//	if _, err = bucket.Attrs(ctx); err != nil {
+//		return nil, nil, err
+//	}
+//
+//	obj := bucket.Object(name)
+//	w := obj.NewWriter(ctx)
+//	if _, err := io.Copy(w, r); err != nil {
+//		return nil, nil, err
+//	}
+//	if err := w.Close(); err != nil {
+//		return nil, nil, err
+//	}
+//
+//
+//	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+//		return nil, nil, err
+//	}
+//
+//	attrs, err := obj.Attrs(ctx)
+//	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+//	return obj, attrs, err
+//}
+//
+//
+//// Save a post to ElasticSearch
+//func saveToES(p *Post, id string) {
+//	// Create a client
+//	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+//	if err != nil {
+//		panic(err)
+//		return
+//	}
+//
+//	// Save it to index
+//	_, err = es_client.Index().
+//		Index(INDEX).
+//		Type(TYPE).
+//		Id(id).
+//		BodyJson(p).
+//		Refresh(true).
+//		Do()
+//	if err != nil {
+//		panic(err)
+//		return
+//	}
+//
+//	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+//}
+//
+//func containsFilteredWords(s *string) bool {
+//	filteredWords := []string{
+//		"fuck",
+//		"150",
+//	}
+//	for _, word := range filteredWords {
+//		if strings.Contains(*s, word) {
+//			return true
+//		}
+//	}
+//	return false
+//}
+//
+//
+//func write(client *storage.Client, bucket, object string) error {
+//	ctx := context.Background()
+//	// [START upload_file]
+//	f, err := os.Open("notes.txt")
+//	if err != nil {
+//		return err
+//	}
+//	defer f.Close()
+//
+//	wc := client.Bucket(bucket).Object(object).NewWriter(ctx)
+//	if _, err = io.Copy(wc, f); err != nil {
+//		return err
+//	}
+//	if err := wc.Close(); err != nil {
+//		return err
+//	}
+//	// [END upload_file]
+//	return nil
+//}
